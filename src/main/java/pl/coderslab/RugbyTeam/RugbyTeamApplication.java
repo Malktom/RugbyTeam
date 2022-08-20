43b273af665cb0901cc56175dc93491aa7948d97
Malktom <tomasz.malkiewicz@yahoo.com>
2022-08-20 23:19:18 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/RugbyTeam/RugbyTeamApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.RugbyTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RugbyTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RugbyTeamApplication.class, args);
+	}
+
+}
